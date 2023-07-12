62673cf7c455d1a428ad237f8b7b41f9857dd6a1
qzhao19 <qi.zhao@outlook.fr>
2023-07-12 23:14:19 +0800

    update

--- kdtree/kd_tree.py
+++ kdtree/kd_tree.py
@@ -1,6 +1,5 @@
 import copy
 import numpy as np
-
 class KDTree(object):
     def __init__(self, data, leaf_size = 10):
         self.data = data
@@ -12,7 +11,7 @@ class KDTree(object):
             raise ValueError("leaf_size must be greater than or equal to 1")
         self.leaf_size = leaf_size
 
-        self.root = []
+        self.tree = []
 
     def _find_partition_axis(self, data):        
         lower_bounds = np.min(data, axis = 0)
@@ -55,13 +54,12 @@ class KDTree(object):
                 (data[n_samples//2:, :], indices[n_samples//2:], 1, 0, False)]
 
         while stack:
-
             # pop data off stack
             data, indices, depth, index, is_left_child = stack.pop()
             n_samples = data.shape[0]
-            node_ptr = len(self.root)
+            node_ptr = len(self.tree)
 
-            _data, _indices, _left_hyper_rect, _right_hyper_rect, left_child, right_child = self.root.pop()
+            _data, _indices, _left_hyper_rect, _right_hyper_rect, left_child, right_child = self.tree.pop()
 
             if is_left_child:
                 self.tree[index] = _data, _indices, _left_hyper_rect, _right_hyper_rect, node_ptr, right_child
@@ -73,15 +71,29 @@ class KDTree(object):
                 _data = data
                 _indices = indices
                 leaf = (_data, _indices, None, None, 0, 0)
-                self.root.append(leaf)
+                self.tree.append(leaf)
             else:
                 partition_axis = self._find_partition_axis(data)
                 cur_indices = np.argsort(data[:,partition_axis], kind='mergesort')
                 data[:,:] = data[cur_indices, :]
                 indices = indices[cur_indices]
-                node_ptr = len(self.root)
+                node_ptr = len(self.tree)
 
+                stack.append((data[:, :n_samples//2], indices[:n_samples//2], depth+1, node_ptr, True))
+                stack.append((data[:, n_samples//2:], indices[n_samples//2:], depth+1, node_ptr, False))
 
+                mid_val = data[n_samples//2, partition_axis]
+                if is_left_child:
+                    left_hyper_rect = _left_hyper_rect.copy()
+                    right_hyper_rect = _left_hyper_rect.copy()
+                else:
+                    left_hyper_rect = _right_hyper_rect.copy()
+                    right_hyper_rect = _right_hyper_rect.copy()
+                
+                left_hyper_rect[1, partition_axis] = mid_val
+                right_hyper_rect[0, partition_axis] = mid_val
+                # append node to tree
+                self.tree.append((None, None, left_hyper_rect, right_hyper_rect, None, None))
 
 
 
